76c257eb01cf88ce1e4ea4fd95d4fc872ca1fdfe
Vikas Periwal <vkperiwal97@gmail.com>
2026-02-05 11:00:01 +0530

    changed card auth logic

--- ATMMachine/models/card.py
+++ ATMMachine/models/card.py
@@ -1,3 +1,4 @@
+from typing import Callable
 from ATMMachine.models.account import Account
 
 
@@ -7,10 +8,17 @@ class Card:
     def __init__(self, number: str, pin: str, account: Account):
         self.id = str(Card._id)
         Card._id += 1
-        
+
+        self._hash_fn: Callable = (
+            hash  # could have also used Argon2 or Bcrypt for better pw safety
+        )
         self.number: str = number
-        self.pin: str = pin
+        self._hashed_pin: str = self._hash_fn(pin)
+
         self.account: Account = account
-    
+
     def __str__(self):
         return f"Card(id={self.id},number={self.number})"
+
+    def auth(self, pin: str) -> bool:
+        return self._hashed_pin == self._hash_fn(pin)
--- ATMMachine/models/card_manager.py
+++ ATMMachine/models/card_manager.py
@@ -2,21 +2,23 @@ from typing import Dict
 from ATMMachine.decorators import singleton
 from ATMMachine.models.card import Card
 
+
 @singleton
 class CardManager:
     def __init__(self):
         self.cards: Dict[str, Card] = dict()
-    
+
     def __str__(self):
         return f"CardManager(mem_address={id(self)}, cards={[card.__str__() for card in self.cards.values()]})"
+
     def add_card(self, card: Card):
         self.cards[card.id] = card
-    
+
     def authenticate_pin(self, card_id: str, pin: str) -> bool:
-        return self.cards[card_id].pin == pin
-    
+        return self.cards[card_id].auth(pin)
+
     def fetch_balance(self, card_id: str) -> float:
         return self.cards[card_id].account.balance
-    
+
     def is_valid_card(self, card_id: str) -> bool:
         return card_id in self.cards
